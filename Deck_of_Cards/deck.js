a9f415cdfd3a87aecacd88d4ded561e472e758dd
Blake C <bcortri1@kent.edu>
2023-05-16 14:45:59 -0400

    Deck of Cards complete

--- Deck_of_Cards/deck.js
+++ Deck_of_Cards/deck.js
@@ -1,1 +1,30 @@
-const axios = require('axios').default;
+var currdeckId ="new"
+$cardBtn = $("#card-btn")
+$cardImg = $("#card")
+
+async function draw_card(deckId=currdeckId){
+    let url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
+    res = await axios.get(url)
+    try{
+        suit = res.data.cards[0].suit
+        val = res.data.cards[0].value
+        img = res.data.cards[0].image
+        console.log(`${val} OF ${suit}`)
+        
+        currdeckId = res.data.deck_id
+        return {"suit":suit, "img":img, "val":val }
+    }
+    catch{
+        console.log(res.data.error)
+        return res
+    }
+}
+
+$cardBtn.on("click", async function(){
+    res = await draw_card()
+    suit = res.suit
+    val = res.val
+    img = res.img
+    $cardImg.attr("src",img)
+    $cardImg.attr("alt",`${val} OF ${suit}`)
+})
