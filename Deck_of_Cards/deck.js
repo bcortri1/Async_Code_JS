5294cf0836de5b271c24bffc87b87dcc4ad1345b
Blake C <bcortri1@kent.edu>
2023-05-26 20:18:56 -0400

    Promise version added

--- Deck_of_Cards/deck.js
+++ Deck_of_Cards/deck.js
@@ -1,8 +1,10 @@
 var currdeckId ="new"
-$cardBtn = $("#card-btn")
+$awaitBtn = $("#await-btn")
+$promiseBtn = $("#promise-btn")
 $cardImg = $("#card")
 
-async function draw_card(deckId=currdeckId){
+//Async/Await version
+async function async_draw_card(deckId=currdeckId){
     let url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
     res = await axios.get(url)
     try{
@@ -20,11 +22,39 @@ async function draw_card(deckId=currdeckId){
     }
 }
 
-$cardBtn.on("click", async function(){
-    res = await draw_card()
-    suit = res.suit
-    val = res.val
-    img = res.img
-    $cardImg.attr("src",img)
-    $cardImg.attr("alt",`${val} OF ${suit}`)
+function displayCard(suit="",val="",img=""){
+    $cardImg.attr("src",img);
+    $cardImg.attr("alt",`${val} OF ${suit}`);
+}
+
+$awaitBtn.on("click", async function(){
+    res = await async_draw_card()
+    displayCard(suit,val,img)
+})
+
+//Promise version
+$promiseBtn.on("click", function(){
+    let deckId=currdeckId
+    let url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
+    let suit = ""
+    let val = ""
+    let img = ""
+
+    axios.get(url)
+    .then(res =>{
+        try{
+            suit = res.data.cards[0].suit
+            val = res.data.cards[0].value
+            img = res.data.cards[0].image
+            console.log(`${val} OF ${suit}`)
+            currdeckId = res.data.deck_id
+            displayCard(suit,val,img)
+        }
+        catch{
+            console.log(res.data.error)
+        }
+    })
+    .catch(res => {
+        console.log(res)
+    })
 })
